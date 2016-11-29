1ab584a6381c92c2d8de10ba90b6855a829e3c5b
Linhua Sun <jshaslh@163.com>
2016-11-29 21:22:42 +0800

    NOTES

---
+++ DAILY_NOTES/2016-11-29.sh
@@ -0,0 +1,273 @@
+## pkubatch barthii__SRR1206370_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206371_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206372_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206373_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206374_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206375_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206376_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206378_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206379_get_snps.batch && sleep 10s
+## pkubatch barthii__SRR1206380_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR1206507_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR1206515_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR1206516_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR1206517_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR1206518_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR1206519_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR3231725_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR3231749_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR3231750_get_snps.batch && sleep 10s
+## pkubatch glaberrima__SRR3231751_get_snps.batch && sleep 10s
+## pkubatch glumaepatula__SRR1264537_get_snps.batch && sleep 10s
+## pkubatch glumaepatula__SRR1997912_get_snps.batch && sleep 10s
+## ##
+## pkubatch glumaepatula__SRR1997915_get_snps.batch && sleep 10s
+## pkubatch glumaepatula__SRX809892_get_snps.batch && sleep 10s
+## pkubatch longistaminata__ERR069041_get_snps.batch && sleep 10s
+## pkubatch longistaminata__ERR069042_get_snps.batch && sleep 10s
+## pkubatch longistaminata__SRR1264538_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069043_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069044_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069045_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069046_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069047_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069048_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069049_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069050_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069051_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069052_get_snps.batch && sleep 10s
+## pkubatch meridionalis__ERR069053_get_snps.batch && sleep 10s
+## pkubatch meridionalis__SRR1264536_get_snps.batch && sleep 10s
+
+
+# pkubatch meridionalis__SRX809898_get_snps.batch && sleep 10s
+# pkubatch punctata__SRR1264539_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068742_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068748_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068764_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068777_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068899_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068983_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068985_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR068987_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR069004_get_snps.batch && sleep 10s
+# pkubatch rufipogon-III__ERR069009_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068632_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068678_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068681_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068682_get_snps.batch && sleep 10s
+
+
+# pkubatch rufipogon-II__ERR068733_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068741_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068784_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068809_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068842_get_snps.batch && sleep 10s
+# pkubatch rufipogon-II__ERR068913_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068722_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068771_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068783_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068833_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068892_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068902_get_snps.batch && sleep 10s
+# pkubatch rufipogon-I__ERR068925_get_snps.batch && sleep 10s
+
+## pkubatch rufipogon-I__ERR068930_get_snps.batch && sleep 10s
+## pkubatch rufipogon-I__ERR068932_get_snps.batch && sleep 10s
+## pkubatch rufipogon-I__ERR069038_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__B039_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__CX11_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__CX307_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__CX367_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__CX4_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__CX578_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__IRIS_313-10850_get_snps.batch && sleep 10s
+## pkubatch sativa_ADM__IRIS_313-11257_get_snps.batch && sleep 10s
+
+## pkubatch sativa_ARO__CX110_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__CX112_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__CX149_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-10851_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-11062_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-11066_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-11258_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-11289_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-11825_get_snps.batch && sleep 10s
+## pkubatch sativa_ARO__IRIS_313-8326_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-10603_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-10852_get_snps.batch && sleep 10s
+
+## pkubatch sativa_AUS__IRIS_313-10861_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-10892_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-11052_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-11053_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-11057_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-11058_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-11191_get_snps.batch && sleep 10s
+## pkubatch sativa_AUS__IRIS_313-11298_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11643_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11722_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11723_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11731_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11791_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11801_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11805_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11819_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-11821_get_snps.batch && sleep 10s
+## pkubatch sativa_IND__IRIS_313-9066_get_snps.batch && sleep 10s
+## pkubatch sativa_JAP__CX262_get_snps.batch && sleep 10s
+## pkubatch sativa_JAP__CX285_get_snps.batch && sleep 10s
+## pkubatch sativa_JAP__IRIS_313-11044_get_snps.batch && sleep 10s
+## pkubatch sativa_JAP__IRIS_313-11046_get_snps.batch && sleep 10s
+
+pkubatch sativa_JAP__IRIS_313-7914_get_snps.batch && sleep 10s
+pkubatch sativa_TEMP__B070_get_snps.batch && sleep 10s
+pkubatch sativa_TEMP__B162_get_snps.batch && sleep 10s
+pkubatch sativa_TEMP__CX142_get_snps.batch && sleep 10s
+pkubatch sativa_TEMP__CX344_get_snps.batch && sleep 10s
+
+pkubatch sativa_TEMP__IRIS_313-10097_get_snps.batch && sleep 10s
+
+# sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TEMP__IRIS_313-11651_get_snps.batch
+# sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TEMP__IRIS_313-11652_get_snps.batch
+# sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TEMP__IRIS_313-11800_get_snps.batch
+# sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TEMP__IRIS_313-8173_get_snps.batch 
+# sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TEMP__IRIS_313-8755_get_snps.batch 
+# 
+# pkubatch sativa_TEMP__IRIS_313-11651_get_snps.batch && sleep 10s
+# pkubatch sativa_TEMP__IRIS_313-11652_get_snps.batch && sleep 10s
+# pkubatch sativa_TEMP__IRIS_313-11800_get_snps.batch && sleep 10s
+# pkubatch sativa_TEMP__IRIS_313-8173_get_snps.batch && sleep 10s
+# pkubatch sativa_TEMP__IRIS_313-8755_get_snps.batch && sleep 10s
+# 
+# grep "short" sativa_TEMP__IRIS_313-11651_get_snps.batch
+# grep "short" sativa_TEMP__IRIS_313-11652_get_snps.batch
+# grep "short" sativa_TEMP__IRIS_313-11800_get_snps.batch
+# grep "short" sativa_TEMP__IRIS_313-8173_get_snps.batch 
+# grep "short" sativa_TEMP__IRIS_313-8755_get_snps.batch 
+## cn-short
+## pkubatch sativa_TROP__CX220_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-10577_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-10582_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-11094_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-11102_get_snps.batch
+
+
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g"  sativa_TROP__CX220_get_snps.batch
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g"  sativa_TROP__IRIS_313-10577_get_snps.batch 
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g"  sativa_TROP__IRIS_313-10582_get_snps.batch 
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g"  sativa_TROP__IRIS_313-11094_get_snps.batch 
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g"  sativa_TROP__IRIS_313-11102_get_snps.batch 
+
+
+
+
+
+
+## cn-short
+## pkubatch sativa_TROP__IRIS_313-11103_get_snps.batch && sleep 10s
+## pkubatch sativa_TROP__IRIS_313-11659_get_snps.batch && sleep 10s
+## pkubatch sativa_TROP__IRIS_313-11755_get_snps.batch && sleep 10s
+## pkubatch sativa_TROP__IRIS_313-11900_get_snps.batch && sleep 10s
+## pkubatch sativa_TROP__IRIS_313-11924_get_snps.batch && sleep 10s
+## 
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TROP__IRIS_313-11103_get_snps.batch
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TROP__IRIS_313-11659_get_snps.batch
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TROP__IRIS_313-11755_get_snps.batch
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TROP__IRIS_313-11900_get_snps.batch
+## sed -i "s/cn-long/cn-short/g;s/dengcnl/dengcns/g" sativa_TROP__IRIS_313-11924_get_snps.batch
+## 
+## pkubatch sativa_TROP__IRIS_313-11103_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-11659_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-11755_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-11900_get_snps.batch
+## pkubatch sativa_TROP__IRIS_313-11924_get_snps.batch
+
+##------
+
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099336/SRR350693_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099337/SRR350694_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099338/SRR350695_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099339/SRR350696_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099340/SRR350697_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099341/SRR350698_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099341/SRR350699_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099342/SRR350700_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099343/SRR350701_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099344/SRR350702_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099345/SRR350703_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099346/SRR350704_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099347/SRR350705_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099348/SRR350706_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099349/SRR350707_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099350/SRR350708_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099351/SRR350709_1.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099336/SRR350693_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099337/SRR350694_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099338/SRR350695_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099339/SRR350696_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099340/SRR350697_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099341/SRR350698_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099341/SRR350699_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099342/SRR350700_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099343/SRR350701_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099344/SRR350702_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099345/SRR350703_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099346/SRR350704_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099347/SRR350705_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099348/SRR350706_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099349/SRR350707_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099350/SRR350708_2.fastq.bz2 .
+mv ftp.ddbj.nig.ac.jp//ddbj_database/dra/fastq/SRA046/SRA046411/SRX099351/SRR350709_2.fastq.bz2 .
+
+#!/bin/bash
+#SBATCH -J QC
+#SBATCH -p cn-short
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=20
+#SBATCH -o QC_%j.out
+#SBATCH -e QC_%j.err
+#SBATCH --no-requeue
+#SBATCH -A deng_g1
+#SBATCH --qos=dengcns
+
+source ~/.bash_profile
+
+cd /lustre1/deng_pkuhpc/slh_RICE_data/Hang-2016-10-06/EBI_NEW/Oryza_brachyanthaGenome_sequencing/BZ2
+
+fastqc -t 20 SRR350693_1.fastq.bz2  SRR350695_2.fastq.bz2  SRR350698_1.fastq.bz2  SRR350700_2.fastq.bz2  SRR350703_1.fastq.bz2  SRR350705_2.fastq.bz2  SRR350708_1.fastq.bz2
+fastqc -t 20 SRR350693_2.fastq.bz2  SRR350696_1.fastq.bz2  SRR350698_2.fastq.bz2  SRR350701_1.fastq.bz2  SRR350703_2.fastq.bz2  SRR350706_1.fastq.bz2  SRR350708_2.fastq.bz2
+fastqc -t 20 SRR350694_1.fastq.bz2  SRR350696_2.fastq.bz2  SRR350699_1.fastq.bz2  SRR350701_2.fastq.bz2  SRR350704_1.fastq.bz2  SRR350706_2.fastq.bz2  SRR350709_1.fastq.bz2
+fastqc -t 20 SRR350694_2.fastq.bz2  SRR350697_1.fastq.bz2  SRR350699_2.fastq.bz2  SRR350702_1.fastq.bz2  SRR350704_2.fastq.bz2  SRR350707_1.fastq.bz2  SRR350709_2.fastq.bz2
+fastqc -t 20 SRR350695_1.fastq.bz2  SRR350697_2.fastq.bz2  SRR350700_1.fastq.bz2  SRR350702_2.fastq.bz2  SRR350705_1.fastq.bz2  SRR350707_2.fastq.bz2
+
+##
+mv SRR350693*.bz2 MMM
+mv SRR350694*.bz2 MMM
+mv SRR350696*.bz2 MMM
+mv SRR350698*.bz2 MMM
+mv SRR350699*.bz2 MMM
+mv SRR350701*.bz2 MMM
+mv SRR350707*.bz2 MMM
+mv SRR350708*.bz2 MMM
+mv SRR350709*.bz2 MMM
+
+##
+
+#!/bin/bash
+#SBATCH -J seq
+#SBATCH -p cn-long
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -o seqtk_%j.out
+#SBATCH -e seqtk_%j.err
+#SBATCH --no-requeue
+#SBATCH -A deng_g1
+#SBATCH --qos=dengcnl
+
+source ~/.bash_profile
+
+for i in *.bz2
+do
+	less ${i} |seqtk trimfq -| python fastx_len_filter.py --min_len 70 |gzip -c > trimmed_modi_${i}.gz
+done
